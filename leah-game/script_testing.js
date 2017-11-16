a6caed06b32f9e51fb749e263f483b7b0b4d8b00
Leo Neil <lnllaurito@yahoo.com>
2017-11-17 00:03:17 +0800

    link updated

--- leah-game/script_testing.js
+++ leah-game/script_testing.js
@@ -1,4 +1,4 @@
-document.getElementById('ok').onmouseover = function() {
+document.getElementById('ok').onhover = function() {
     document.getElementById('ok').style.left = (Math.random() * 1000) + "px";
     document.getElementById('ok').style.top = (Math.random() * 200) + "px";
-}               
+}
